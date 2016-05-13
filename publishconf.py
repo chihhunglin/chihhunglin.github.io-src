de7acc42effd267514e640c69e2c1acada5254c5
Peter Lin <dragon270329@gmail.com>
2016-05-13 12:16:49 +0800

    feat: disqus

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
+DISQUS_SITENAME = "chihhunglinblog"
 GOOGLE_ANALYTICS = "UA-57479725-2"
